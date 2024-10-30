009de5f07ba989391353a030ea2339a8c59b57cd
Anitha-26 <anithas2677@gmail.com>
2024-10-30 19:47:07 +0530

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,40 +2,27 @@ import React from 'react'
 import "./About.css"
 
 function About() {
-  return (
-    <div>
-
-            <div className='About'>
-                    <div style={{width:'60%'}}>
-                        <h1>About Me</h1>
-                        <hr style={{backgroundColor:'yellow',height:'.5px',width:'100%'}}></hr>
-                       <div className='about-section'>
-                           <div style={{width:'300px'}}>
-                                  <h4>Name : Arulmozhivarman S</h4>
-                                  <h4>Date of Birth : January 15,2005</h4>
-                                  <h4>Address : Tamilnadu,India</h4>
-                                  <h4>Zipcode : 637207</h4>
-                                  <h4>Email : arul150105@gmail.com</h4>
-                                  <h4>Phone : +91-8144774437</h4>
-                            
-                           </div>
-                            <div style={{width:'300px'}}>
-                                  <p>
-                                    Hello! I’m Arulmozhivarman S, a passionate Full Stack Developer and an IT student at
-                                    Sri Krishna College of Engineering and Technology, Coimbatore. 
-                                    <br></br>
-                                    <br></br>
-                                    Currently in the final year of my degree,
-                                    <br></br>
-                                    <br></br>
-                                    I’ve been immersing myself in the ever-evolving world of technology and web development.
-                                    </p>
-                            </div>
-                       </div>
-                    </div>
-                    {/* <button style={{width:'100px'}}><a href='mailto:arul150105@gmail.com'> <b>Mail Me</b></a></button> */}
-            </div>
-
+  return(
+    <div style={{paddingBottom:'50px'}}>
+        <div className='About'>
+            <section >
+                <div>
+                  {/* <img style={{width:'300px',height:'400px'}} src=''/> */}
+                </div>
+                <div>
+                  <h1 style={{color:'grey'}}>About Me</h1>
+                </div>
+                <div className='aboutmepara'>
+                    <p>
+                   
+                    Hello! I'm a third-year Information Technology student at Sri Krishna College of Engineering and Technology (SKCET) in Coimbatore. At 19 years old, I’m driven by a passion for problem-solving, technology, and innovation. My journey in IT began with a curiosity for understanding how things work and has evolved into a dedicated pursuit of technical proficiency and creativity.
+                    </p>
+                    <p>
+                    Currently, I’m focused on building a solid foundation in software development, data structures, and computer systems. My coursework has provided me with experience in programming languages such as Java, C++, and I’m continuously working to deepen my understanding of algorithms, web development, and database management.
+                    </p>
+                </div>
+            </section>
+        </div>
     </div>
   )
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,6 +3,7 @@ import "./Homepage.css"
 import { Link } from 'react-router-dom'
 import About from './About'
 import Skills from './Skills'
+import Projects from './Projects'
 
 function Homepage() {
   return (
@@ -10,35 +11,37 @@ function Homepage() {
         <div className='Navbar'> 
             <ul className='Navbar-ul'>
                 <a href="#skills" >Skills</a>
-                <a href='/projects'><li>Projects</li></a>
+                <a href='#project'><li>Projects</li></a>
                 <a href='#about'><li>About</li></a>
             </ul>
         </div>
         <div className='Main-div' >
                 <img data-aos="fade-up" data-aos-duration="1000" style={{width:'100px',height:'100px'}} src='https://cdn-icons-png.flaticon.com/256/5046/5046943.png'></img>
                
-                <h2 data-aos="fade-up" data-aos-duration="1500">Hi,I'am Arulmozhivarman</h2>
+                <h2 data-aos="fade-up" data-aos-duration="1500">Hi,I'am Arulmozhivarman S</h2>
                 <h1 data-aos="fade-up" data-aos-duration="2000">
                 <span style={{color:'#624E88'}}>Full Stack Web Developer</span> Crafting Dynamic,
                  Scalable, and <br></br>User-Friendly Web Applications
                 </h1>
                 <h3 data-aos="fade-up" data-aos-duration="2500">
-                Expertise in building modern web applications using technologies like JavaScript, React, Node.js, and MySql. I’m passionate about solving complex problems and delivering seamless user experiences.
+                Expertise in building modern web applications using technologies like JavaScript, React, Springboot, and MySql. I’m passionate about solving complex problems and delivering seamless user experiences.
                 </h3>
                 <div data-aos="fade-up" data-aos-duration="3000" className='Connect'>
                         <a href='https://www.linkedin.com/feed/'><img style={{width:'30px',height:'30px'}} src='https://cdn-icons-png.flaticon.com/128/3536/3536505.png'></img></a>
-                        <a href='/mail'><img style={{width:'30px',height:'30px'}} src='https://cdn-icons-png.flaticon.com/128/270/270798.png'></img></a>
-                        <a href='#'><img style={{width:'30px',height:'30px'}} src='https://cdn-icons-png.flaticon.com/128/2530/2530217.png'></img></a>
-                        <a href='#'><img style={{width:'30px',height:'30px'}} src='https://cdn-icons-png.flaticon.com/128/15713/15713420.png'></img></a>
+                        <a href='https://github.com/Arulmozhivarman-S'><img style={{width:'30px',height:'30px'}} src='https://cdn-icons-png.flaticon.com/128/270/270798.png'></img></a>
+                        <a href='mailto:arul150105@gmail.com'><img style={{width:'30px',height:'30px'}} src='https://cdn-icons-png.flaticon.com/128/2530/2530217.png'></img></a>
+                        <a href='https://www.instagram.com/arul__14/'><img style={{width:'30px',height:'30px'}} src='https://cdn-icons-png.flaticon.com/128/15713/15713420.png'></img></a>
                 </div>
         </div>
       <div id='skills' data-aos="fade-up" data-aos-duration="1000">
             <Skills/>
       </div>
+      <div id='project' data-aos="fade-up"  >
+          <Projects/>
+      </div>
       <div id='about' style={{color:'gray'}} data-aos="fade-up" data-aos-duration="1000">
             <About />
       </div>
-
       
     </div>
   )
