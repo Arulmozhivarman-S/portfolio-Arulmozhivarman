47f8f9d7e8bf45c958dd058a977b0349aa3e379c
Anitha-26 <anithas2677@gmail.com>
2026-06-28 19:07:11 +0530

    updated

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./About.css"
+
+function About() {
+  return(
+    <div style={{paddingBottom:'50px'}}>
+        <div className='About'>
+            <section >
+                <div>
+                  {/* <img style={{width:'300px',height:'400px'}} src=''/> */}
+                </div>
+                <div>
+                  <h1 style={{color:'#624E88'}} >About Me</h1>
+                </div>
+                <div className='aboutmepara'>
+                    <p>
+                    Hello! I'm a 21 years old, I’m driven by a passion for problem-solving, technology, and innovation. My journey in IT began with a curiosity for understanding how things work and has evolved into a dedicated pursuit of technical proficiency and creativity.
+                    I did my Bachlors in Btech-IT from Sri krishna college of engineering and technology coimbatore. my CGPA is 7.9
+                    </p>
+                    <p>
+                    Currently, I’m focused on building a solid foundation in software development, data structures, and computer systems. My coursework has provided me with experience in programming languages such as Java, C++, and I’m continuously working to deepen my understanding of algorithms, web development, and database management.
+                    </p>
+                </div>
+            </section>
+        </div>
+    </div>
+  )
+}
+
+export default About
